d83bd43ff8aa99b7939c6cbad350db04aee574b4
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2023-02-28 12:04:18 -0300

    fiz um novo hook para excluir um evento

---
+++ src/state/hooks/useExcluirEvento.ts
@@ -0,0 +1,12 @@
+import { useSetRecoilState } from 'recoil';
+import { IEvento } from "../../interfaces/IEvento";
+import { listaDeEventosState } from '../atom';
+
+const useExcluirEvento = (): any => {
+  const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState);
+  return (evento: IEvento) => {
+    return setListaDeEventos(listaAntiga => [...listaAntiga.filter(evt => evt.id !== evento.id)]);
+  }
+}
+
+export default useExcluirEvento;
